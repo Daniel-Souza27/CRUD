f853507a26171bdf1d6c7bb19081c15409d03e83
Daniel Souza Filho <danieldsfilho@gmail.com>
2022-02-06 21:45:04 -0300

    a

---
+++ css/arrow.js
@@ -0,0 +1,16 @@
+
+  
+// declaracao da arrow function 
+const getData = () =>{
+    const data = {}
+    data.name = 'Daniel'
+    data.lastName = 'S. Filho'
+    return data
+}
+  
+// chama a funcao 
+console.log(getData())
+
+// output: 
+// { name: 'Daniel', lastName: 'S. Filho' }
+ 
